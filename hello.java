886b8440970fc579f3a00e09bb75930b9d406cb7
saichethanr <saichethan9019@gmail.com>
2024-07-10 18:50:19 +0530

    primitive data types

--- hello.java
+++ hello.java
@@ -1,5 +1,16 @@
 public class hello {
     public static void main(String args[]){
+
+        //variables
         System.out.println(2+8);
+        int num1=10;
+        int num2 = 18;
+        int result  = num1 + num2;
+        System.out.println(result);
+
+
+        //data types
+        
+
     }
 }
