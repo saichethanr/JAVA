2c2d356f43694cfbbf67dfbb158ecff0fdad9efb
saichethanr <saichethan9019@gmail.com>
2024-07-15 09:07:02 +0530

    addition done

--- oops/intro.java
+++ oops/intro.java
@@ -2,14 +2,14 @@ package oops;
 
 class calc{
     public  int add(int a,int b){
-        System.out.println("add it");
-        return -1;
+       return a+b;
     }
 }
 public class intro {
     public static void main(String args[]){
           calc c = new calc();
-          c.add(1, 5);
+          int ans  =  c.add(1, 5);
+          System.out.println(ans);
           
     }
 }
