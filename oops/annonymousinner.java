5a49d8ec5ff4d61f63ebb8a7976a02fa1155eb03
saichethanr <saichethan9019@gmail.com>
2024-07-18 22:42:53 +0530

    annonuymous class

---
+++ oops/annonymousinner.java
@@ -0,0 +1,27 @@
+package oops;
+
+
+class A{
+    public void show(){
+        System.out.println("sai");
+    }
+}
+
+// after compilation both the . class files are created 
+class B extends A{
+    public void show(){
+        System.out.println("goobe");
+    }
+}
+public class annonymousinner {
+    public static void main(String[] args) {
+        //this is the new days 
+        //inner class
+        A obj = new A(){
+            public void show(){
+                System.out.println("new show");
+            }
+        };
+        obj.show();
+    }
+}
