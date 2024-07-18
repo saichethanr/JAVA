b0ba32ac6f7176ac56bc3faad49e80f3936d27c1
saichethanr <saichethan9019@gmail.com>
2024-07-18 23:21:27 +0530

    need for interface

---
+++ oops/interface_info/interface_need.java
@@ -0,0 +1,29 @@
+package oops.interface_info;
+
+//use of an abstract class or an interface would work
+abstract class  computer {
+    public abstract void code(computer comp);
+}
+class Laptop extends computer
+{
+  public void code(computer comp){
+    System.out.println("code in lap");
+  }
+}
+
+class desktop extends  computer
+{
+  public void code(computer comp){
+    System.out.println("code in lap");
+  }
+}
+
+public class interface_need {
+    public static void main(String[] args) {
+        computer obj1 = new Laptop();
+        computer obj2 = new desktop();
+
+        obj1.code(obj1);
+        obj2.code(obj1);
+    }
+}
