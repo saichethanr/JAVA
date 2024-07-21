4cec96514230998d80630f582a76295582b4dd56
saichethanr <saichethan9019@gmail.com>
2024-07-21 23:52:46 +0530

    threads runnable

--- threads/thread_runnable.java
+++ threads/thread_runnable.java
@@ -1,39 +1,5 @@
 package threads;
 
-class A implements Runnable
-{
-	public void run()
-	{
-		for(int i=1;i<=100;i++)
-		{
-			System.out.println("Hi");
-
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-		}
-	}
-}
-
-
-// the runnable interface will not have a thread methods
-class B implements Runnable
-{
-	public void run()
-	{
-		for(int i=1;i<=100;i++)
-		{
-			System.out.println("Hello");
-            try {
-                Thread.sleep(5);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-		}
-	}
-}
 public class thread_runnable {
     public static void main(String[] args) {
         //lambda expressions
@@ -51,7 +17,18 @@ public class thread_runnable {
                 }
             };
        
-        Runnable obj2 = new B();
+        Runnable obj2 = () ->
+        {
+            for(int i=1;i<=100;i++)
+            {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
         
         Thread t1 = new Thread(obj1);
         Thread t2 = new Thread(obj2);
