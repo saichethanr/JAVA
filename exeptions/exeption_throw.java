4db693d459dbfa672392d40a6cd56f5fda6fe492
saichethanr <saichethan9019@gmail.com>
2024-07-21 17:22:12 +0530

    exeption throw

---
+++ exeptions/exeption_throw.java
@@ -0,0 +1,38 @@
+package exeptions;
+
+
+//force fully calling the catch block to handle the exeption is called throw
+public class exeption_throw {
+
+        public static void main(String[] args) {
+            
+    //    	int i=2;
+            int i=0;
+            int j=0;
+     
+            
+            try
+            {
+                j=18/i;
+                if(j==0)
+                    throw new ArithmeticException("I don't want to do print zero");
+            }
+    
+            catch(ArithmeticException e)
+            {
+                j=18/i;
+                System.out.println("that is default output"+e);
+    
+    //    		System.out.println("Cannot divide by zero");
+            }
+            
+            catch(Exception e)
+            {
+      
+                System.out.println("Something went wrong."+e);
+            }
+            System.out.println(j);
+            System.out.println("Bye");
+        }
+
+}
