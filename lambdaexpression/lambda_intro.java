0f5fd2330541ead5a8ce3701f0221a6a3faae9c7
saichethanr <saichethan9019@gmail.com>
2024-07-21 16:16:50 +0530

    lambda _into

---
+++ lambdaexpression/lambda_intro.java
@@ -0,0 +1,32 @@
+package lambdaexpression;
+
+
+
+//functional interface as ther eis only signle method
+//use of annotatiion can be usefull
+@FunctionalInterface
+ interface A {
+  void show(int n);
+    
+}
+
+// class B  implements A{
+//     public void show(){
+//         System.out.println("in show");
+//     }
+// }
+public class lambda_intro {
+    public static void main(String[] args) {
+        //this is lambda expression
+        A obj = (int n) ->{
+                System.out.println("in show");
+                System.out.println("this is for multiline statements" + " " + n);
+        };
+        //sigle line statements
+        A obj1 = (int n) ->  System.out.println("in show");
+        
+    
+        obj.show(6);
+        obj1.show(7);
+    }
+}
