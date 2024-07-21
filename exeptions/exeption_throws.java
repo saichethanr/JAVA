3a21537928be13eba4be1fbb63311b675e3b378b
saichethanr <saichethan9019@gmail.com>
2024-07-21 17:59:57 +0530

    exeption throws

---
+++ exeptions/exeption_throws.java
@@ -0,0 +1,48 @@
+package exeptions;
+
+//what does throws do it just pushes the responsibility of handling the exeption to its parent
+class A
+{
+    //main method calls show so main method should handle it
+	public void show() throws ClassNotFoundException
+	{
+//    	try
+//    	{
+//    		Class.forName("Calc");
+//    	}
+//    	catch(ClassNotFoundException e)
+//    	{
+//    		System.out.println("Not able to find theh class");
+//    	}
+		
+		Class.forName("Calc");
+	}
+}
+
+public class exeption_throws {
+    static {
+		System.out.println("Class Loader");
+	}
+	
+    public static void main(String[] args) {
+    	
+//    	try
+//    	{
+//    		Class.forName("Class");
+//    	}
+//    	catch(ClassNotFoundException e)
+//    	{
+//    		System.out.println("Not able to find theh class");
+//    	}
+    	
+    	A obj=new A();
+    	try {
+    		obj.show();
+    	}catch(ClassNotFoundException e)
+    	{
+    		e.printStackTrace();
+    	}
+    	
+    	
+    }
+}
