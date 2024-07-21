4c0e206f27d3b1db49e244279c03096cc9525bcf
saichethanr <saichethan9019@gmail.com>
2024-07-21 23:41:31 +0530

    runnable

---
+++ threads/threads_intro.java
@@ -0,0 +1,44 @@
+package threads;
+
+class A extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Hi");
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+		}
+	}
+}
+
+class B extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Hello");
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+		}
+	}
+}
+public class threads_intro {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+        obj2.setPriority(Thread.MAX_PRIORITY);
+
+        obj1.start();
+        obj2.start();
+    }
+}
