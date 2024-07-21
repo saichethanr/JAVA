f9c0f56b1c1bad708e96c644d3433b9bde3c51c1
saichethanr <saichethan9019@gmail.com>
2024-07-21 12:04:10 +0530

    enum class

---
+++ abt_enum/enum_class.java
@@ -0,0 +1,42 @@
+package abt_enum;
+
+enum Laptop{
+    //	Mackbook(2000), XPS(2200), Surface(1500), ThinkPad(1800);
+        Mackbook(2000), XPS(2200), Surface, ThinkPad(1800);
+        
+        private int price;
+        
+        private Laptop()
+        {
+            price=500;
+        }
+        //the constructors scope is given to be private as the objects are declared inside the enum itself
+        private Laptop(int price)
+        {
+            this.price=price;
+        }
+        
+        public int getPrice()
+        {
+            return price;
+        }
+        public void setPrice(int price)
+        {
+            this.price=price;
+            System.out.println("in Laptop" + this.name());
+        }
+    }
+
+public class enum_class {
+    public static void main(String[] args) {
+
+        //    	Laptop lap=Laptop.Mackbook;
+        //    	System.out.println(lap+ " : "+lap.getPrice());
+                
+                for(Laptop lap : Laptop.values())
+                {
+                    System.out.println(lap+" : "+lap.getPrice());
+                }
+            }
+
+}
