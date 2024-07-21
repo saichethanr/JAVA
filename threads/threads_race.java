2fe4780b214536c7f251f9ba87614fff08ab8d72
saichethanr <saichethan9019@gmail.com>
2024-07-22 00:03:04 +0530

    mutual exclusion

---
+++ threads/threads_race.java
@@ -0,0 +1,43 @@
+package threads;
+
+
+class Counter{
+    int cnt;
+    //use of synchronized will make sure that java will call the increment only once
+    public synchronized void increment(){
+        cnt++;
+    }
+}
+public class threads_race {
+    public static void main(String[] args) throws InterruptedException {
+
+         Counter c = new Counter();
+
+        //lambda expressions
+        Runnable obj1 = () ->
+            {
+              for(int i=0;i<=100;i++){
+                    c.increment();
+              }
+            };
+       
+        Runnable obj2 = () ->
+        {
+              for(int i=0;i<=100;i++){
+                c.increment();
+              }
+        };
+        
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
+        
+        t1.join();
+        t2.join();
+          
+        //both t1 and t2 are trying to access the count in the same time 
+        System.out.println(c.cnt);
+    }
+}
