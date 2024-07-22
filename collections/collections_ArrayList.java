955ac922e4531803a15b35461841ecca472e2685
saichethanr <saichethan9019@gmail.com>
2024-07-22 11:38:41 +0530

    collection arraylist

--- collections/collections_ArrayList.java
+++ collections/collections_ArrayList.java
@@ -5,17 +5,20 @@ import java.util.List;
 
 public class collections_ArrayList {
     public static void main(String[] args) {
+        //collection does not use indexing
         Collection <Integer> nums  = new ArrayList <Integer>();
         nums.add(6);
         nums.add(2);
         nums.add(7);
         System.out.println(nums);
 
-
+        //list uses indesx to work with 
         List <Integer> num = new ArrayList <Integer>();
         nums.add(6);
         nums.add(2);
         nums.add(7);
         System.out.println(num.get(1));
+        System.out.println(num.indexOf(2));
+
     }
 }
