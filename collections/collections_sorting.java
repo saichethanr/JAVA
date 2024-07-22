9af43d436f12d2793f316d55105a8f3687095ef1
saichethanr <saichethan9019@gmail.com>
2024-07-22 13:20:07 +0530

    collections sorting

--- collections/collections_sorting.java
+++ collections/collections_sorting.java
@@ -6,6 +6,38 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+
+//auto sorting already mentioning in the class
+//use comparable in the class itself
+//or define the comparator in the main metjhod for this class
+//these are the two methods available to sort a class
+class Student implements Comparable<Student>{
+    int age;
+    String name;
+
+
+    
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+
+    @Override
+    public int compareTo(Student that) {
+        if(this.age>that.age){
+            return 1;
+         }
+         else{
+            return -1;
+         }
+    }
+
+    
+}
 public class collections_sorting {
     public static void main(String[] args) {
         //basic sorting
@@ -63,6 +95,31 @@ public class collections_sorting {
         System.out.println(nums);
 
 
+       
 
+        //we can also use the lambda expression to solve tthe comparator
+        //List of students
+        List<Student> stud = new ArrayList<>();
+        stud.add(new Student(29, "sai"));
+        stud.add(new Student(90, "shilpa"));
+        stud.add(new Student(27, "kimiko"));
+
+        for(Student s: stud){
+            System.out.println(s);
+        }
+
+        //sort these values based on their age
+        Comparator <Student> compa = new Comparator<Student>() {
+            @Override
+            public int compare(Student i, Student j) {
+                //if returns 1 then  it swaps if it dosent return 1 it dosent swap
+             if(i.age>j.age){
+                return 1;
+             }
+             else{
+                return -1;
+             }
+            }
+        };
     }
 }
