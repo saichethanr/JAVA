6cdf332477c8011c62213bd53601a5aadd5747a4
saichethanr <saichethan9019@gmail.com>
2024-08-17 08:36:27 +0530

    done

---
+++ collections/test.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class test {
+    public static void main(String args[]){
+        Collection <Integer> cln = new ArrayList<Integer>();
+        Collection <Integer> c2 = new HashSet<Integer>();
+          
+    }
+}
